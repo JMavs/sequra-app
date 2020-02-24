df2b4ce7752522a2a5cb8097347927ec76c0f6e0
Juan Garrido Arcos <jgarrido@theboton.io>
2020-02-24 08:14:54 +0100

    test: added first test to orders

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid with empty attributes" do
+    expect(Order.new).not_to be_valid
+  end
 end

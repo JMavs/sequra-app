e802510cdd2b2fef170799616054642a5340aa35
Juan Garrido Arcos <jgarrido@theboton.io>
2020-02-24 08:09:23 +0100

    chore: changed text in merchants tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it "is valid with empty attributes" do
+  it "is not valid with empty attributes" do
     expect(Merchant.new).not_to be_valid
   end
 end

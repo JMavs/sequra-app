7d2d646f8803e91c61e18f56d90cc9c44e251239
Juan Garrido Arcos <jgarrido@theboton.io>
2020-02-24 09:19:08 +0100

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rules_data_info = [
+  {
+    min_amount: 0.01,
+    max_amount: 49.99,
+    percentage: 1
+  },
+  {
+    min_amount: 50,
+    max_amount: 300,
+    percentage: 0.95
+  },
+  {
+    min_amount: 300.01,
+    max_amount: nil,
+    percentage: 0.85
+  }
+]
+disburse_rules = DisburseRule.create(rules_data_info)
